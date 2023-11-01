a020e3581e467a3c929a3fbd0d703fecda2a8890
Piotr Rzeczkowski <rzeczkowski.piotr@gmail.com>
2023-11-01 11:36:58 +0100

    Switch to routing

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'screens/home_screen.dart';
+import 'constants.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,27 +13,15 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Mr Hangman',
+      title: AppConfig.name,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.purple),
         useMaterial3: true,
       ),
-      home: const HomeScreen(title: 'Mr Hangman'),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => HomeScreen(title: AppConfig.name)
+      },
     );
   }
 }
