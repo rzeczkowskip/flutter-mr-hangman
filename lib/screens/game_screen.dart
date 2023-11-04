2239953d16e584dc84fe00a72d1915b1e4fbc522
Piotr Rzeczkowski <rzeczkowski.piotr@gmail.com>
2023-11-04 13:00:07 +0100

    Create game screen containers

--- lib/screens/game_screen.dart
+++ lib/screens/game_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import '../components/game_phrase.dart';
+import '../components/game_keyboard.dart';
 
 class GameScreen extends StatefulWidget {
   const GameScreen({super.key});
@@ -11,18 +13,18 @@ class _GameScreenState extends State<GameScreen> {
   Future<bool> _onWillPop() async {
     return (await showDialog(
           context: context,
-          builder: (context) => new AlertDialog(
-            title: new Text('Are you sure you want to end the game?'),
-            content: new Text(
+          builder: (context) => AlertDialog(
+            title: const Text('Are you sure you want to end the game?'),
+            content: const Text(
                 'You will loose all progress and current score will be save to high scores.'),
             actions: <Widget>[
               TextButton(
                 onPressed: () => Navigator.of(context).pop(false),
-                child: new Text('No'),
+                child: const Text('No'),
               ),
               TextButton(
                 onPressed: () => Navigator.of(context).pop(true),
-                child: new Text('Yes'),
+                child: const Text('Yes'),
               ),
             ],
           ),
@@ -37,7 +39,51 @@ class _GameScreenState extends State<GameScreen> {
     return WillPopScope(
         onWillPop: _onWillPop,
         child: Scaffold(
-          body: SafeArea(child: Text('Game area')),
+          body: SafeArea(
+            child: Column(
+              children: [
+                const Text('game status'),
+                Expanded(
+                  child: OrientationBuilder(builder: (context, orientation) {
+                    Axis direction = orientation == Orientation.portrait
+                        ? Axis.vertical
+                        : Axis.horizontal;
+
+                    List<Widget> gameBoxes = [
+                      Expanded(
+                        flex: 1,
+                        child: Container(
+                          alignment: Alignment.center,
+                          margin: const EdgeInsets.all(20),
+                          child: const Text('Image'),
+                        ),
+                      ),
+                      Expanded(
+                        flex: 0,
+                        child: Container(
+                          alignment: Alignment.center,
+                          margin:
+                          const EdgeInsets.symmetric(vertical: 30, horizontal: 10),
+                          child: const GamePhrase(phrase: 'demo p__a_e to g____'),
+                        ),
+                      ),
+                    ].toList();
+
+                    return Flex(
+                      direction: direction,
+                      children: orientation == Orientation.portrait
+                          ? gameBoxes
+                          : (gameBoxes.reversed.toList()),
+                    );
+                  }),
+                ),
+                Container(
+                  margin: const EdgeInsets.symmetric(vertical: 20),
+                  child: GameKeyboard(usedLetters: const [], onTap: (letter) {}),
+                )
+              ],
+            ),
+          ),
         ));
   }
 }
