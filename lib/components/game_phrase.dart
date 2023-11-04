6f9488ebf310fd55324843832d8a70eafa3bd0af
Piotr Rzeczkowski <rzeczkowski.piotr@gmail.com>
2023-11-04 12:12:27 +0100

    Remove phrase masking from widget

--- lib/components/game_phrase.dart
+++ lib/components/game_phrase.dart
@@ -1,34 +1,14 @@
 import 'package:flutter/material.dart';
 
-class GamePhrase extends StatefulWidget {
-  GamePhrase({
-    super.key,
-    required this.phrase,
-    required this.usedLetters,
-  });
+class GamePhrase extends StatelessWidget {
+  GamePhrase({ required this.phrase });
 
-  List<String> usedLetters;
-  String phrase;
-
-  @override
-  State<GamePhrase> createState() => _GamePhraseState();
-}
-
-class _GamePhraseState extends State<GamePhrase> {
-  String _maskedPhrase() {
-    final String allowedChars = ' ' + widget.usedLetters.join();
-    final RegExp allowedCharsRegexp = RegExp(r'(?:[^\w\d]|[^' + allowedChars + '])');
-    
-    print({allowedChars, allowedCharsRegexp});
-
-    return widget.phrase.replaceAll(allowedCharsRegexp, '_');
-  }
+  final String phrase;
 
   @override
   Widget build(BuildContext context) {
-    print({widget.phrase, widget.usedLetters, _maskedPhrase()});
     return Text(
-      _maskedPhrase(),
+      phrase,
       style: TextStyle(
         fontSize: 42,
         letterSpacing: 16,
