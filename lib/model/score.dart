70aeb2459059258ff025121b3d3f45fa66d35e8f
Piotr Rzeczkowski <rzeczkowski.piotr@gmail.com>
2023-11-22 23:08:21 +0100

    Add json serialization

--- lib/model/score.dart
+++ lib/model/score.dart
@@ -1,6 +1,15 @@
 class Highscore {
-  Highscore({required this.date, required this.score});
+  Highscore(this.date, this.score);
 
   final DateTime date;
   final int score;
+
+  Highscore.fromJson(Map<String, dynamic> data)
+      : date = DateTime.parse(data['date']),
+        score = data['score'] as int;
+
+  Map<String, String> toJson() => {
+        'date': date.toIso8601String(),
+        'score': score.toString(),
+      };
 }
